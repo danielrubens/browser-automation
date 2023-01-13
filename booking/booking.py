663284bda966834ee9241859b2e01ae2c97eeaef
danielrubens <danielrubensdaniel@gmail.com>
2023-01-13 00:32:22 -0300

    exception raised to currency change

--- booking/booking.py
+++ booking/booking.py
@@ -1,5 +1,6 @@
 import booking.constants as const
 import os
+import time
 from selenium import webdriver
 
 
@@ -17,23 +18,41 @@ class Booking(webdriver.Chrome):
             self.quit()
 
     def land_first_page(self):
+        self.implicitly_wait(15)
         self.get(const.BASE_URL)
+        time.sleep(3)
 
     def choose_currency(self, currency=None):
-        self.implicitly_wait(15)
-        currency_element = self.find_element_by_css_selector(
-            'button[data-tooltip-text="Choose your currency"]'
-        )
-        currency_element.click()
-        selected_currency = self.find_element_by_css_selector(
-            f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'
-        )
-        selected_currency.click()
+        try:
+            self.implicitly_wait(5)
+            currency_element = self.find_element_by_css_selector(
+                'button[data-tooltip-text="Choose your currency"]'
+            )
+            currency_element.click()
+            selected_currency = self.find_element_by_css_selector(
+                f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'
+            )
+            selected_currency.click()
+            time.sleep(3)
+        except:
+            print('HTML Element not loaded')
 
     def select_place(self, place):
+        self.implicitly_wait(15)
         search_field = self.find_element_by_id('ss')
         search_field.clear()
         search_field.send_keys(place)
 
         first_result = self.find_element_by_css_selector('li[data-i="0"]')
         first_result.click()
+        time.sleep(3)
+
+    def select_dates(self, checkin, checkout):
+        self.implicitly_wait(15)
+        checkin_element = self.find_element_by_css_selector(f'td[data-date="{checkin}"]')
+        checkin_element.click()
+        print(checkin_element)
+        print(checkin)
+        checkout_element = self.find_element_by_css_selector(f'td[data-date="{checkout}"]')
+        checkout_element.click()
+        time.sleep(3)
--- run.py
+++ run.py
@@ -4,3 +4,4 @@ with Booking() as bot:
     bot.land_first_page()
     bot.choose_currency(currency='USD')
     bot.select_place('New York')
+    bot.select_dates('2023-01-12', '2023-01-15')
