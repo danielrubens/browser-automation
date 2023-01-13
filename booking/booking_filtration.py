d3b3b07db2b90335906574313b7b0730102492eb
danielrubens <danielrubensdaniel@gmail.com>
2023-01-13 14:04:47 -0300

    ty

--- booking/booking_filtration.py
+++ booking/booking_filtration.py
@@ -2,6 +2,11 @@
 That will be responsible to interact with our website
 after we have some results, to apply friltrations.'''
 
+from selenium.webdriver.remote.webdriver import WebDriver
+
 class BookingFiltration:
-    def __init__(self, driver):
-        pass
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+    
+    def apply_star_rating(self):
+        self.driver.find_element_by_id('filter_class')
