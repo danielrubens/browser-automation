009f4d3d9df2bab6a66e5bcfdfcca969495526af
danielrubens <danielrubensdaniel@gmail.com>
2023-01-14 03:18:32 -0300

    input to eliminate hardcore strings

--- run.py
+++ run.py
@@ -4,11 +4,11 @@ import time
 try:
     with Booking() as bot:
         bot.land_first_page()
-        # bot.choose_currency(currency='USD')
-        # bot.select_place('New York')
-        # bot.select_dates('2023-01-15', '2023-01-19')
-        # bot.select_guests(1)
-        # bot.search()
+        bot.choose_currency(currency='USD')
+        bot.select_place(input("Where do you want to go?"))
+        bot.select_dates(input("Checkin date"), input("Checkout date"))
+        bot.select_guests(input("How many guests?"))
+        bot.search()
         bot.apply_filtrations(5, 'price')
         time.sleep(5)
         bot.refresh()
