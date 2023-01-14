b453d922229cd765d22e14cc4bf740c9ef05b368
danielrubens <danielrubensdaniel@gmail.com>
2023-01-13 21:19:18 -0300

    try except on context manager

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 from booking.booking import Booking 
 import time
 
-
-with Booking() as bot:
-    bot.land_first_page()
-    # bot.choose_currency(currency='USD')
-    # bot.select_place('New York')
-    # bot.select_dates('2023-01-15', '2023-01-19')
-    # bot.select_guests(1)
-    # bot.search()
-    bot.apply_filtrations(5, 'price')
-    time.sleep(5)
+try:
+    with Booking() as bot:
+        bot.land_first_page()
+        # bot.choose_currency(currency='USD')
+        # bot.select_place('New York')
+        # bot.select_dates('2023-01-15', '2023-01-19')
+        # bot.select_guests(1)
+        # bot.search()
+        bot.apply_filtrations(5, 'price')
+        time.sleep(5)
+except Exception as e:
+    print(f'Found exception: ${e}')
     
