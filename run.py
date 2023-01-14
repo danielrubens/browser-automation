1b33d737c0c434a3b8ea93f499801b1bdef06cf7
danielrubens <danielrubensdaniel@gmail.com>
2023-01-13 23:28:56 -0300

    raise exception

--- run.py
+++ run.py
@@ -12,5 +12,14 @@ try:
         bot.apply_filtrations(5, 'price')
         time.sleep(5)
 except Exception as e:
-    print(f'Found exception: ${e}')
-    
+    if 'in PATH' in str(e):
+      print(
+        'You are trying to run the bot from command line \n'
+        'Please add to PATH your Selenium Drivers \n'
+        'Windows: \n'
+        '        set PATH=%PATH;C:path-to-your-folder \n \n'
+        'Linux: \n'
+        '       PATH=%PATH:/path/toyour/folder/ \n'
+      )
+    else:
+        raise    
